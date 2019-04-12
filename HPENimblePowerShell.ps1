105147f22da78e4ec6a620fefb8c1b8ff4672fe4
Chris Lionetti <chris@lionetti.com>
2019-04-12 13:54:53 -0700

    cleansed code

--- HPENimblePowerShell.ps1
+++ HPENimblePowerShell.ps1
@@ -1,38 +1,32 @@
-
-
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 if (-not ([System.Management.Automation.PSTypeName]'ServerCertificateValidationCallback').Type)
 {   $certCallback = @"
-        using System;
-        using System.Net;
-        using System.Net.Security;
-        using System.Security.Cryptography.X509Certificates;
-        public class ServerCertificateValidationCallback
-        {   public static void Ignore()
-            {   if(ServicePointManager.ServerCertificateValidationCallback ==null)
-                {   ServicePointManager.ServerCertificateValidationCallback += 
-                    delegate    (   Object obj, 
-                                    X509Certificate certificate, 
-                                    X509Chain chain, 
-                                    SslPolicyErrors errors
-                                )
-                    {   return true;
-                    };
-            }
+    using System;
+    using System.Net;
+    using System.Net.Security;
+    using System.Security.Cryptography.X509Certificates;
+    public class ServerCertificateValidationCallback
+    {   public static void Ignore()
+        {   if(ServicePointManager.ServerCertificateValidationCallback ==null)
+            {   ServicePointManager.ServerCertificateValidationCallback += 
+                delegate    (   Object obj, 
+                                X509Certificate certificate, 
+                                X509Chain chain, 
+                                SslPolicyErrors errors
+                            )
+                {   return true;
+                };
         }
     }
+}
 "@
 Add-Type $certCallback
 }
 [ServerCertificateValidationCallback]::Ignore()
-$uri='https://raw.githubusercontent.com/chris-lionetti/HPENimbleStorageAzureStack/master/HPENimbleStorage.ps1'
+$uri="https://raw.githubusercontent.com/chris-lionetti/HPENimbleStorageAzureStack/master/NimbleStorageUnattended.ps1"
+$Code=(Invoke-WebRequest -Uri $uri -Method Get).content
+mkdir C:\NimbleStorage
+$Code | out-file -FilePath "C:\NimbleStorage\NimbleStorageUnAttended.ps1"
 
-mkdir C:\temp
-Get-process | Out-File "C:\temp\MyOutputFile.txt"
-
-$Code=$(Invoke-WebRequest -Uri $uri -Method Get).content
-$Code | out-file -FilePath 'C:\temp\MyCode.txt'
-
-$R=Invoke-WebRequest -Uri $uri
-$R | Out-File -FilePath 'C:\temp\MyResponse.txt'
-
+$RunOnce="HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce"
+# set-itemproperty $RunOnce "NextRun" ('C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe -executionPolicy Unrestricted -File ' + 'C:\NimbleStorage\NimbleStorageUnattended.ps1')
--- NimbleStorageUnattended.ps1
+++ NimbleStorageUnattended.ps1
@@ -1,28 +1,30 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-if (-not ([System.Management.Automation.PSTypeName]'ServerCertificateValidationCallback').Type)
-{   $certCallback = @"
-    using System;
-    using System.Net;
-    using System.Net.Security;
-    using System.Security.Cryptography.X509Certificates;
-    public class ServerCertificateValidationCallback
-    {   public static void Ignore()
-        {   if(ServicePointManager.ServerCertificateValidationCallback ==null)
-            {   ServicePointManager.ServerCertificateValidationCallback += delegate (   Object obj, 
-                                                                                        X509Certificate certificate, 
-                                                                                        X509Chain chain, 
-                                                                                        SslPolicyErrors errors
-                                                                                    )
-                {   return true;
-                };
+function Set-NSASSecurityProtocolOverride
+{   # Will override the behavior of Invoke-WebRequest to allow access without a Certificate. 
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    if (-not ([System.Management.Automation.PSTypeName]'ServerCertificateValidationCallback').Type)
+    {   $certCallback = @"
+            using System;
+            using System.Net;
+            using System.Net.Security;
+            using System.Security.Cryptography.X509Certificates;
+            public class ServerCertificateValidationCallback
+            {   public static void Ignore()
+                {   if(ServicePointManager.ServerCertificateValidationCallback ==null)
+                    {   ServicePointManager.ServerCertificateValidationCallback += delegate (   Object obj, 
+                                                                                                X509Certificate certificate, 
+                                                                                              X509Chain chain, 
+                                                                                             SslPolicyErrors errors
+                                                                                           )
+                        {   return true;
+                        };
+                    }
+                }
             }
-        }
-    }
 "@
-Add-Type $certCallback
+        Add-Type $certCallback
+    }
+    [ServerCertificateValidationCallback]::Ignore()
 }
-[ServerCertificateValidationCallback]::Ignore()
-
 function PostEvent([String]$TextField, [string]$EventType)
 {   # Subroutine to Post Events to Log/Screen/EventLog
     $outfile = "C:\NimbleStorage\Logs\NimbleInstall.log" 
@@ -47,36 +49,57 @@ function PostEvent([String]$TextField, [string]$EventType)
             $testfield | out-file -filepath $outfile -append
         }
 } 
+function Load-NSASAzureModules
+{   # Loads all of the Nimble Storage Azure Stack specific PowerShell Modules
+    if (-not (Get-Module -name AzureRM.Storage) )
+    {   post-event "The required AzureStack Powershell are being Installed" "Info"
+        Import-Module -Name PowerShellGet
+        Import-Module -Name PackageManagement  
+        Register-PsRepository -Default
+        Install-Packageprovider -name NuGet -MinimumVersion 2.8.5.201 -force
+        register-psrepository -default
+        install-module AzureRM -RequiredVersion 2.4.0 -Force
+        Install-Module -name AzureStack -RequiredVersion 1.7.1 -Force
+        # Install the Azure.Storage module version 4.5.0
+        Install-Module -Name Azure.Storage -RequiredVersion 4.5.0 -Force -AllowClobber
 
-$uri='https://raw.githubusercontent.com/chris-lionetti/HPENimbleStorageAzureStack/master/HPENimbleStorage.ps1'
-$ForceReboot=$False
-$DidSomething=$False
+        # Install the AzureRm.Storage module version 5.0.4
+        Install-Module -Name AzureRM.Storage -RequiredVersion 5.0.4 -Force -AllowClobber
 
-# Step 0 If Nimble PSTK not downloaded download it
-if ( Test-Path 'C:\Windows\System32\WindowsPowerShell\v1.0\Modules\HPENimblePowerShellToolkit' -PathType Container )
+        # Remove incompatible storage module installed by AzureRM.Storage
+        Uninstall-Module Azure.Storage -RequiredVersion 4.6.1 -Force
+
+        # Load the modules explicitly specifying the versions
+        Import-Module -Name Azure.Storage -RequiredVersion 4.5.0
+        Import-Module -Name AzureRM.Storage -RequiredVersion 5.0.4
+    } else 
+    {   post-event "The required AzureStack Powershell Modules have been detected" "Info"        
+    }
+}
+function Load-NimblePSTKModules
+{   # Loads the Nimble PowerShell Toolkit from the GitHub Site identifed in the Global Variables
+    if ( Test-Path 'C:\Windows\System32\WindowsPowerShell\v1.0\Modules\HPENimblePowerShellToolkit' -PathType Container )
     {   PostEvent "The HPE NimbleStorage PowerShell Toolkit is installed" "Information"
     } else 
     {   PostEvent "Now Installing the Nimble PowerShell Toolkit" "Warning"
         $DidSomething=$True
-        $uri='https://github.com/chris-lionetti/HPENimbleStorageAzureStack/raw/master/HPENimblePowerShellToolkit.210.zip'
-        invoke-webrequest -uri $uri -outfile "C:\NimbleStorage\HPENimblePowerShellToolkit.210.zip"
+        invoke-webrequest -uri $NimblePSTKuri -outfile "C:\NimbleStorage\HPENimblePowerShellToolkit.210.zip"
         $PSMPath="C:\Windows\System32\WindowsPowerShell\v1.0\Modules"
-        expand-archive -path "C:\NimbleStorage\HPENimblePowerShellToolkit.210.zip" -DestinationPath $PSMPath
+        expand-archive -path "C:\NimbleStorage\HPENimblePowerShellToolkit.210.zip" -DestinationPath $WindowsPowerShellModulePath
     }
 
-# step 0b Use secrets to discover array and log into array 
-
-# Ensure that iSCSI is started;
-Start-Service msiscsi
-Set-Service msiscsi -startuptype "automatic"
-PostEvent "Ensuring that the iSCSI Initiator Service is started, and setting it to start automatically" "Warning"
-
-# Step 1 Detect if MPIO is installed
-if( (get-WindowsFeature -name "Multipath-io").installed )
+}
+function Load-WindowsMPIOFeature
+{   # Load the Windows MPIO feature. Returns True if a Reboot is required.
+    if( (get-WindowsFeature -name "Multipath-io").installed )
     {   PostEvent "The Windows Multipath IO Feature is already Insatlled" "Information"
         if ( (get-windowsFeature -name "Multipath-io").InstallState -ne "Installed")
             {   PostEvent "Reboot is required after a Windows Multipath IO Feature Installation" "Warning"
                 $ForceReboot=$True
+                return $True
+            } else 
+            {   post-event "The Windows Multipath IO Feature does not require a reboot" "Information"
+                return $false                
             }
     } else 
     {  # Step 1a Install MPIO if not installed
@@ -85,15 +108,18 @@ if( (get-WindowsFeature -name "Multipath-io").installed )
         PostEvent "Reboot is required after a Windows Multipath IO Feature Installation" "Warning"
         $ForceReboot=$True
         $DidSomething=$True
+        return $true
     }
-# Step 2 If NWT not downloaded, download it
-if ( -not $ForceReboot )
-{   $software="Nimble Windows Toolkit"
+}
+function Load-NWTPackage
+{   # Download and instlal the Nimble Windows Toolkit. If already installed, return false, otherwise install and request a reboot.
+    $NWTsoftware="Nimble Windows Toolkit"
     $installed = (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where { $_.DisplayName -eq $software }) -ne $null
     if ($installed)
     {   PostEvent "The Nimble Windows Toolkit is already installed" "Information"
+        return $false
     } else
-    {   # Step 2a If NWT not installed, silent install it
+    {   # If NWT not installed, silent install it
         $uri='https://github.com/chris-lionetti/HPENimbleStorageAzureStack/raw/master/Setup-NimbleNWT-x64.5.0.0.7991.exe'
         invoke-webrequest -uri $uri -outfile "C:\NimbleStorage\Setup-NimbleNWT-x64.5.0.0.7991.exe"
         $NWTEXE = "C:\NimbleStorage\Setup-NimbleNWTx64.0.0.0.XXX.exe"
@@ -105,15 +131,48 @@ if ( -not $ForceReboot )
         & $NWTEXE $NWTArg1 $NWTArg2 $NWTArg3 $NWTArg4 $NWTArg5
         # Invoke-Command -ScriptBlock "C:\NimbleStorage\Setup-NimbleNWTx64.0.0.0.XXX.exe EULAACCEPTED=Yes HOTFIXPASS=Yes RebootYesNo=Yes NIMBLEVSSPORT=Yes /silent"
         PostEvent "Initiating download and Silent Installation of the Nimble Windows Toolkit" "Warning"
-        $DidSomething=$True
-        $ForceReboot=$True
+        return $true
     }
 }
-if ($DidSomething -or $ForceReboot)
-    {   $RunOnce="HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce"
-        # set-itemproperty $RunOnce "NextRun" ('C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe -executionPolicy Unrestricted -File ' + 'C:\NimbleStorage\NimbleStorageUnattended.ps1')
-        PostEvent "This Installation Script is set to run again once the server has been rebooted. Please Reboot this server" "Error"
-    } else 
-    {   PostEvent "This Script has verified that all required software is installed, and that no reboot is needed" "Information"
-        PostEvent "This script will not be re-run on reboot" "warning"        
-    }
+
+#####################################################################################################################################################
+# MAIN Unattended Installation Script for Nimble Storag on Azure Stack.                                                                             #
+#####################################################################################################################################################
+# Set the Global Variables needed for the script to operate
+    $uri='https://raw.githubusercontent.com/chris-lionetti/HPENimbleStorageAzureStack/master/HPENimbleStorage.ps1'
+    $NimblePSTKuri='https://github.com/chris-lionetti/HPENimbleStorageAzureStack/raw/master/HPENimblePowerShellToolkit.210.zip'
+    $WindowsPowerShellModulePath="C:\Windows\System32\WindowsPowerShell\v1.0\Modules"
+    $ForceReboot=$False
+    $NimbleArrayIP="10.1.240.20"
+    $NimbleUser="admin"
+    $NimblePassword="admin"
+
+# Step 1. Load the Azure Stack Specific PowerShell modules
+    Load-NSASAzureModules 
+# Step 2. All all Invoke-Web* commands to operate without a certificate. 
+    Set-NSASSecurityProtocolOverride
+# Step 3. Download and install the Nimble PowerShell Toolkit
+    Load-NimblePSTKModules
+# step 3b. TODO: Use secrets to discover array and log into array 
+    $NimbleArrayIP="10.1.240.20"
+    $NimbleUser="admin"
+    $NimblePassword="admin"
+# Step 4. Ensure that iSCSI is started;
+    Start-Service msiscsi
+    Set-Service msiscsi -startuptype "automatic"
+    PostEvent "Ensuring that the iSCSI Initiator Service is started, and setting it to start automatically" "Warning"
+# Step 5. Detect if MPIO is installed. If it needs reboot, set the flag as the return
+    $ForceReboot=Load-WindowsMPIOFeature
+# Step 6. If NWT not downloaded, download it. If it is downloaded and installed require reboot, otherwise to do not set reboot flag
+    if (-not $ForceReboot) 
+        {   $ForceReboot=Load-NWTPackage
+        }
+# Step 7. If the ForceReboot flag is set, make this script run at the next reboot, otherise exit successfully/complete.
+    if ($ForceReboot)
+        {   $RunOnce="HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce"
+            #set-itemproperty $RunOnce "NextRun" ('C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe -executionPolicy Unrestricted -File ' + 'C:\NimbleStorage\NimbleStorageUnattended.ps1')
+            PostEvent "This Installation Script is set to run again once the server has been rebooted. Please Reboot this server" "Error"
+        } else 
+        {   PostEvent "This Script has verified that all required software is installed, and that no reboot is needed" "Information"
+            PostEvent "This script will NOT be re-run on reboot" "warning"        
+        }
