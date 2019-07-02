90b8a8c67ff8942cb671c9d23a5d7cc8d22a4431
Chris Lionetti <chris@lionetti.com>
2019-07-02 20:40:10 +0000

    Cleaned up the extra bits.
    Took out the numerous extra comments, cleaned up the output.

--- HPENimblePowerShell.ps1
+++ HPENimblePowerShell.ps1
@@ -5,7 +5,6 @@ $NimbleArrayIP = "10.1.240.20"
 # This single file should NOT be posted to YOUR public Github site as it contains the username and password of the Nimble array
 # that you intend to use.
 ################################################################################################################################
-
 $InitialPull=@'
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 if (-not ([System.Management.Automation.PSTypeName]'ServerCertificateValidationCallback').Type)
@@ -42,14 +41,11 @@ write-host "Hit CTRL-C in next 60 seconds to abort the AutoReboot cycle"
 start-sleep -Seconds 60
 shutdown -t 0 -r -f
 '@
-
 mkdir C:\NimbleStorage -ErrorAction SilentlyContinue 
-
 New-Item -Path "HKLM:\Software\AzureStackNimbleStorage"
 New-ItemProperty -Path "HKLM:\Software\AzureStackNimbleStorage" -PropertyType String -Name NimbleUserName -Value $NimbleUserName
 New-ItemProperty -Path "HKLM:\Software\AzureStackNimbleStorage" -PropertyType String -Name NimblePassword -Value $NimblePassword
 New-ItemProperty -Path "HKLM:\Software\AzureStackNimbleStorage" -PropertyType String -Name NimbleArrayIP -Value $NimbleArrayIP
-
 out-file -filepath C:\NimbleStorage\InitialPull.ps1 -inputobject $InitialPull -force
 $RunOnce="HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce"
 set-itemproperty $RunOnce "NextRun" ('C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe -executionPolicy Unrestricted -File ' + 'C:\NimbleStorage\InitialPull.ps1')
--- NimbleStorageUnattended.ps1
+++ NimbleStorageUnattended.ps1
@@ -1,37 +1,23 @@
 #################################################################
-# Unattended installation script for connecting AzureStack to   #
-# to a Nimble Storage Infrastructure.                           #
-#                                                               #
-# All functions use the Verb-Nouns contruct, but the Noun is    #
-# always preceeded by AZNS which stands for AzureStack Nimble   #
-# Storage. This prevents collisions in customer enviornments    #                                                      
-#                                                               #
-# Additionally, this script will automatically create a LOG     #
-# directoy at C:\NimbleStorage\Logs and will also post to the   #
-# Windows Event Logs.                                           #
-#                                                               #
-# Published https://raw.githubusercontent.com/chris-lionetti/HPENimbleStorageAzureStack/master/NimbleStorageUnattended.ps1
-# Written by Chris Lionetti                                     #
-#################################################################
+# Unattended installation script for connecting AzureStack to to a Nimble Storage Infrastructure.                           #
+# This script will automatically create a LOG directoy at C:\NimbleStorage\Logs
+
 
 # Variable Block
 $NWTuri=            'https://github.com/chris-lionetti/HPENimbleStorageAzureStack/raw/master/Setup-NimbleNWT-x64.6.0.1.3252.exe'
 $NimblePSTKuri=     'https://github.com/chris-lionetti/HPENimbleStorageAzureStack/raw/master/HPENimblePowerShellToolkit.300.zip'   
-$WindowsPowerShellModulePath="C:\Windows\System32\WindowsPowerShell\v1.0\Modules"
-# $NimbleArrayIP=     "10.1.240.20"
-# $NimbleUser=        "admin"
-# $NimblePassword=    "admin"
-$AZNSoutfile =      "C:\NimbleStorage\Logs\NimbleInstall.log"
-$RunOnce=           "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce"
-$ScriptLocation=    'C:\NimbleStorage\NimbleStorageUnattended.ps1'
-$RunOnceValue=      'C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe -executionPolicy Unrestricted -File ' + $ScriptLocation
 $UpdatedPSTKcmd=    'https://raw.githubusercontent.com/chris-lionetti/HPENimbleStorageAzureStack/master/AzureStack.ps1'
 $UpdatedPSTK=       'https://raw.githubusercontent.com/chris-lionetti/HPENimbleStorageAzureStack/master/NimPSSDK.psm1'
 
-$NimbleUser = (Get-ItemProperty -Path HKLM:\Software\AzureStackNimbleStorage).NimbleUserName
-$NimblePassword=(Get-ItemProperty -Path HKLM:\Software\AzureStackNimbleStorage).NimblePassword
-$NimbleArrayIP = (Get-ItemProperty -Path HKLM:\Software\AzureStackNimbleStorage).NimbleArrayIP
-write-host "Your username is $NimbleUser and password is $NimblePassword and IP Address is $NimbleArrayIP"
+# No variables below this line need to be modified by the user/consumer of the script
+$AZNSoutfile =      "C:\NimbleStorage\Logs\NimbleInstall.log"
+$WindowsPowerShellModulePath="C:\Windows\System32\WindowsPowerShell\v1.0\Modules"
+$ScriptLocation=    'C:\NimbleStorage\NimbleStorageUnattended.ps1'
+$RunOnceValue=      'C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe -executionPolicy Unrestricted -File ' + $ScriptLocation
+$RunOnce =          "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce"
+$NimbleUser =       (Get-ItemProperty -Path HKLM:\Software\AzureStackNimbleStorage).NimbleUserName
+$NimblePassword =   (Get-ItemProperty -Path HKLM:\Software\AzureStackNimbleStorage).NimblePassword
+$NimbleArrayIP =    (Get-ItemProperty -Path HKLM:\Software\AzureStackNimbleStorage).NimbleArrayIP
 
 function Post-AZNSEvent([String]$AZNSTextField, [string]$AZNSEventType)
 {   # Subroutine to Post Events to Log/Screen/EventLog
@@ -44,7 +30,6 @@ function Post-AZNSEvent([String]$AZNSTextField, [string]$AZNSEventType)
             default     { $AZNScolor="gray" }
         }
     write-host "- "$AZNStextfield -foregroundcolor $AZNScolor
-    Write-Eventlog -LogName Application -Source NimbleStorage -EventID 1 -Message $AZNSTextField -EntryType $AZNSEventType -Computername "." -category 0
     $AZNSTextField | out-file -filepath $AZNSoutfile -append
 } 
 function Set-NSASSecurityProtocolOverride
@@ -73,68 +58,6 @@ function Set-NSASSecurityProtocolOverride
         Add-Type $certCallback
     }
     [ServerCertificateValidationCallback]::Ignore()
-}
-function Setup-ASNSLogEvents([String] $AZNSaction, [String]$AZNSStep )
-{   # Valid options for the action are either "StartLog" or "EndLog" or defining a step number
-    if ( $AZNSaction -like "StartLog")
-        {   # Create the location for the log file
-            if (! (test-path $AZNSoutfile))
-                {   mkdir c:\NimbleStorage -erroraction SilentlyContinue
-                    mkdir c:\NimbleStorage\Logs -erroraction SilentlyContinue
-                }
-            # This banner makes it easier to parse the log file later quickly.
-            "##########################################################" | out-file -filepath $AZNSoutfile -append
-            "################### Starting New Run #####################" | out-file -filepath $AZNSoutfile -append
-            if (! (test-path HKLM:\SYSTEM\CurrentControlSet\Services\Eventlog\application\NimbleStorage) )
-                {   # if the eventlog catagory doesnt exist, need to register it
-                    New-Eventlog -LogName Application -source NimbleStorage
-                    Post-AZNSEvent "Creating Eventlog\Application\NimbleStorage Eventlog Source" "Warning"
-                }
-        }
-    if ( $AZNSaction -like "EndLog")
-        {   "################### Ending Run #####################" | out-file -filepath $AZNSoutfile -append
-            "####################################################" | out-file -filepath $AZNSoutfile -append
-        }
-    if ( $AZNSAction -like "Step")
-        {   "# Now Executing Step "+$AZNSStep | out-file -filepath $AZNSoutfile -append
-
-        }
-}
-function Load-NSASAzureModules
-{   # Loads all of the Nimble Storage Azure Stack specific PowerShell Modules
-    if (-not (get-Module -name AzureRM.Storage -ErrorAction SilentlyContinue) )
-    {   Post-AZNSEvent "The required AzureStack Powershell are being Installed" "Info"
-        Install-Packageprovider -name NuGet -MinimumVersion 2.8.5.201 -force
-            Post-AZNSEvent "Install-Packageprovider -name NuGet -MinimumVersion 2.8.5.201" "Info"
-        Set-PSRepository -name "PSGallery" -InstallationPolicy Trusted
-            Post-AZNSEvent "Set-PSRepository -name PSGallery -InstallationPolicy Trusted" "Info"
-        Import-Module -Name PowerShellGet
-            Post-AZNSEvent "Import-Module -Name PowerShellGet" "Info"
-        Set-PSRepository -name "PSGallery" -InstallationPolicy Trusted
-            Post-AZNSEvent "Set-PSRepository -name PSGallery -InstallationPolicy Trusted" "info"
-        Import-Module -Name PackageManagement  
-            Post-AZNSEvent "Import-Module -Name PackageManagement" "Info"
-        Register-PsRepository -Default -ErrorAction SilentlyContinue
-            Post-AZNSEvent "Register-PsRepository -Default -ErrorAction SilentlyContinue" "Info"
-        register-psrepository -default -ErrorAction SilentlyContinue
-            Post-AZNSEvent "register-psrepository -default -ErrorAction SilentlyContinue" "Info"
-        install-module AzureRM -RequiredVersion 2.4.0
-            Post-AZNSEvent "install-module AzureRM -RequiredVersion 2.4.0" "Info"
-        Install-Module -name AzureStack -RequiredVersion 1.7.1
-            Post-AZNSEvent "Install-Module -name AzureStack -RequiredVersion 1.7.1" "Info"
-        Install-Module -Name Azure.Storage -RequiredVersion 4.5.0 -Force -AllowClobber
-            Post-AZNSEvent "Install-Module -Name Azure.Storage -RequiredVersion 4.5.0 -Force -AllowClobber" "Info"
-        Install-Module -Name AzureRM.Storage -RequiredVersion 5.0.4 -Force -AllowClobber
-            Post-AZNSEvent "Install-Module -Name AzureRM.Storage -RequiredVersion 5.0.4 -Force -AllowClobber" "Info"
-        Uninstall-Module Azure.Storage -RequiredVersion 4.6.1 -Force
-            Post-AZNSEvent "Uninstall-Module Azure.Storage -RequiredVersion 4.6.1 -Force" "Info"
-        Import-Module -Name Azure.Storage -RequiredVersion 4.5.0
-            Post-AZNSEvent "Import-Module -Name Azure.Storage -RequiredVersion 4.5.0" "Info"
-        Import-Module -Name AzureRM.Storage -RequiredVersion 5.0.4
-            Post-AZNSEvent "Import-Module -Name AzureRM.Storage -RequiredVersion 5.0.4" "Info"
-    } else 
-    {   Post-AZNSEvent "The required AzureStack Powershell Modules have been detected" "Info"        
-    }
 }
 function Load-NimblePSTKModules
 {   # Loads the Nimble PowerShell Toolkit from the GitHub Site identifed in the Global Variables
@@ -217,12 +140,6 @@ function Configure-AZNSiSCSI
 function Create-AZNSNimbleInitiatorGroups
 {   # The Autogenerated Initiator Group will be named for the servers hostname
     $MyLocalIQN=(Get-InitiatorPort | where-object {$_.ConnectionType -like "iSCSI"} ).nodeaddress
-    # $MyNimUsername=(Get-ItemProperty -Path HKCU:\Software\NimbleStorage\Credentials\DefaultCred).UserName
-    # Post-AZNSEvent $MyNimUsername+" is the Useranme" "Info"
-    # $MyNimPassword=(Get-ItemProperty -Path HKCU:\Software\NimbleStorage\Credentials\DefaultCred).Password
-    # Post-AZNSEvent $MyNimPassword+" is the Password" "Info"
-    # $NimblePasswordObect = ConvertTo-SecureString $MyNimPassword -AsPlainText -force
-    # $NimbleCredObject = new-object -typename System.Management.Automation.PSCredential -argumentlist $MyNimUsername, $NimblePasswordObect
     Import-Module HPENimblePowerShellToolkit
     if (Test-NSNimbleWindowsToolkitInstalledConfigured)
     {   Connect-AzNSGroup -UserNWTCredentials $true -IgnoreServerCertificate
@@ -249,35 +166,6 @@ function Create-AZNSNimbleInitiatorGroups
     {   Post-AZNSEvent "NWT was not installed or configured, will setup initiators on next reboot" "Warning"
     }
 }
-<# 
-function Store-AZNSCreds
-{   # Create the Registry Entries where the Credentials can be stored. Only available to 'this user'
-    If (! (Test-Path "HKCU:\Software\NimbleStorage\Credentials" -ErrorAction SilentlyContinue) )
-        {   Write-Host -ForegroundColor Red "Credentials Path Not Found."
-            New-Item -Path "HKCU:\Software\NimbleStorage" -Name "Credentials" -Force
-            Post-AZNSEvent "Creating Registry Key to store credentials at HKCU:\Software\NimbleStorage\Credentials" "Info"
-        }
-    if (! (Test-Path "HKCU:\Software\NimbleStorage\Credentials\DefaultCred") )
-        {   New-Item -Path HKCU:\Software\NimbleStorage\Credentials\DefaultCred
-            Post-AZNSEvent "Creating Registry Key to store credentials at HKCU:\Software\NimbleStorage\Credentials\DefaultCred" "Info"
-        }   
-    if (! (Get-ItemProperty -Path HKCU:\Software\NimbleStorage\Credentials\NimbleStorage\DefaultCred -name UserName -ErrorAction SilentlyContinue) )
-        {   New-ItemProperty -Path HKCU:\Software\NimbleStorage\Credentials\DefaultCred -PropertyType String -Name UserName -Value $NimbleUser -ErrorAction SilentlyContinue
-            Post-AZNSEvent "Storing credential username under HKCU:\Software\NimbleStorage\Credentials\DefaultCred" "Info"
-        }    
-    if (! (Get-ItemProperty -Path HKCU:\Software\NimbleStorage\Credentials\NimbleStorage\DefaultCred -name Password -ErrorAction SilentlyContinue) )
-        {   New-ItemProperty -Path HKCU:\Software\NimbleStorage\Credentials\DefaultCred -PropertyType String -Name Password -Value $NimblePassword -ErrorAction SilentlyContinue
-            Post-AZNSEvent "Storing credential password under HKCU:\Software\NimbleStorage\Credentials\DefaultCred" "Info"
-        } 
-    if (! (Get-ItemProperty -Path HKCU:\Software\NimbleStorage\Credentials\NimbleStorage\DefaultCred -name IPAddress -ErrorAction SilentlyContinue) )
-        {   New-ItemProperty -Path HKCU:\Software\NimbleStorage\Credentials\DefaultCred -PropertyType String -Name IPAddress -Value $NimbleArrayIP -ErrorAction SilentlyContinue
-            Post-AZNSEvent "Storing credential IP Address under HKCU:\Software\NimbleStorage\Credentials\DefaultCred" "Info"
-        } 
-    Post-AZNSEvent "To obtain username use Username = (Get-ItemProperty -Path HKCU:\Software\NimbleStorage\Credentials\NimbleStorage\DefaultCred).UserName" "Info"
-    Post-AZNSEvent "To obtain password use Password = (Get-ItemProperty -Path HKCU:\Software\NimbleStorage\Credentials\NimbleStorage\DefaultCred).Password" "Info"
-    Post-AZNSEvent "To obtain password use IPAddress= (Get-ItemProperty -Path HKCU:\Software\NimbleStorage\Credentials\NimbleStorage\DefaultCred).IPAddress" "Info"
-}
-#>
 
 function Setup-AZNSNimbleWindowsToolkit
 {   #Configure the NWT with the supplied Username and Password.
@@ -289,12 +177,7 @@ function Setup-AZNSNimbleWindowsToolkit
             if ( Get-NWTConfiguration | where{$_.GroupMgmtIPList -ne ""} )
                 {   Post-AZNSEvent "The Nimble Windows Toolkit has already been configured" "info"
                 } else 
-                {   # $MyNimUsername=(Get-ItemProperty -Path HKCU:\Software\NimbleStorage\Credentials\DefaultCred).UserName
-                    # Post-AZNSEvent $MyNimUsername+" is the Useranme" "Info"
-                    # $MyNimPassword=(Get-ItemProperty -Path HKCU:\Software\NimbleStorage\Credentials\DefaultCred).Password
-                    # Post-AZNSEvent $MyNimPassword+" is the Password" "Info"
-
-                    $NimblePasswordObect = ConvertTo-SecureString $NimblePassword -AsPlainText -force
+                {   $NimblePasswordObect = ConvertTo-SecureString $NimblePassword -AsPlainText -force
                     $NimbleCredObject = new-object -typename System.Management.Automation.PSCredential -argumentlist $NimbleUser, $NimblePasswordObect
                     set-nwtconfiguration -groupmgmtip $NimbleArrayIP -Credential $NimbleCredObject
                     Post-AZNSEvent "The Nimble Windows Toolkit has been Configured" "info"
@@ -302,57 +185,28 @@ function Setup-AZNSNimbleWindowsToolkit
         } 
 }
 
-#####################################################################################################################################################
-# MAIN Unattended Installation Script for Nimble Storag on Azure Stack.                                                                             #
-#####################################################################################################################################################
-# Set the Global Variables needed for the script to operate
-    
-# Step 1. Lets Add a Header to the Log File
-    Setup-ASNSLogEvents Startlog
-    Setup-ASNSLogEvents Step 1
-# Step 2. Load the Azure Stack Specific PowerShell modules
-    Setup-ASNSLogEvents Step 2
-    # Load-NSASAzureModules 
-# Step 3. All all Invoke-Web* commands to operate without a certificate. 
-    Setup-ASNSLogEvents Step 3
-    Set-NSASSecurityProtocolOverride
-# Step 4. Download and install the Nimble PowerShell Toolkit
-    Setup-ASNSLogEvents Step 4
-    Load-NimblePSTKModules
-# Step 5. Store the Credentials to the array for future automation.
-    Setup-ASNSLogEvents Step 5
-    # Store-AZNSCreds
-# Step 6. Ensure that iSCSI is started; 
-    Setup-ASNSLogEvents Step 6
-    Configure-AZNSiSCSI
-# Step 7. Configure the Nimble Array to have the correct Initiator Group and Initiator    
-    Setup-ASNSLogEvents Step 7
-    Create-AZNSNimbleInitiatorGroups
-# Step 8. Detect if MPIO is installed. If it needs reboot, set the flag as the return
-    Setup-ASNSLogEvents Step 8
-    $ForceReboot=Load-WindowsMPIOFeature
-# Step 9. If NWT not downloaded, download it. If it is downloaded and installed require reboot, otherwise to do not set reboot flag
-    Setup-ASNSLogEvents Step 9
-    $ForceReboot=Load-NWTPackage
-# Step 10. Configur the NWT using PowerShell
-    Setup-ASNSLogEvents Step 10
-    Setup-AZNSNimbleWindowsToolkit
-# Step A. If the ForceReboot flag is set, make this script run at the next reboot, otherise exit successfully/complete.
-    Setup-ASNSLogEvents Step 11
-    if ($ForceReboot)
-        {   set-itemproperty -path $RunOnce "NextRun" $RunOnceValue
-            Post-AZNSEvent "This Installation Script is set to run again once the server has been rebooted. Please Reboot this server" "Warning"
-        } else 
-        {   if (Get-ItemProperty -Path $RunOnce) 
-                {   remove-itemproperty -path $RunOnce "NextRun"
-                    Post-AZNSEvent "This script will NOT be re-run on reboot" "warning"        
-                }
-            Post-AZNSEvent "This Script has verified that all required software is installed, and that no reboot is needed" "Information"     
-        }
-    Setup-ASNSLogEvents Endlog 
-    if ($ForceReboot)
-        {   write-host "Hit CTRL-C in next 60 seconds to abort the AutoReboot cycle"
-            start-sleep -Seconds 60
-            shutdown -t 0 -r -f
-        }
+##########################################################################
+# MAIN Unattended Installation Script for Nimble Storag on Azure Stack.  #
+Set-NSASSecurityProtocolOverride
+Load-NimblePSTKModules
+Configure-AZNSiSCSI
+Create-AZNSNimbleInitiatorGroups
+$ForceReboot=Load-WindowsMPIOFeature
+$ForceReboot=Load-NWTPackage
+Setup-AZNSNimbleWindowsToolkit
+if ($ForceReboot)
+    {   set-itemproperty -path $RunOnce "NextRun" $RunOnceValue
+        Post-AZNSEvent "This Installation Script is set to run again once the server has been rebooted. Please Reboot this server" "Warning"
+    } else 
+    {   if (Get-ItemProperty -Path $RunOnce) 
+            {   remove-itemproperty -path $RunOnce "NextRun"
+                Post-AZNSEvent "This script will NOT be re-run on reboot" "warning"        
+            }
+        Post-AZNSEvent "This Script has verified that all required software is installed, and that no reboot is needed" "Information"     
+    }
+if ($ForceReboot)
+    {   write-host "Hit CTRL-C in next 60 seconds to abort the AutoReboot cycle"
+        start-sleep -Seconds 60
+        shutdown -t 0 -r -f
+    }
     
